f1ae7e61410b0dc38517b8b1a7659468fcdc22b7
Keijiro Takahashi <keijiro@gmail.com>
2016-12-08 16:38:52 +0900

    Initial commit.

---
+++ Assets/Test/RigController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Klak.Motion;
+using System.Collections;
+
+public class RigController : MonoBehaviour
+{
+    [SerializeField] float _interval = 3;
+    [SerializeField] Transform[] _targets;
+
+    IEnumerator Start()
+    {
+        var follower = GetComponent<SmoothFollow>();
+
+        yield return null;
+
+        for (var i = 0;; i = (i + 1) % _targets.Length)
+        {
+            RehashPivot(_targets[i]);
+            follower.target = _targets[i];
+            yield return new WaitForSeconds(_interval);
+        }
+    }
+
+    void RehashPivot(Transform root)
+    {
+        foreach (var shaker in root.GetComponentsInChildren<BrownianMotion>())
+            shaker.Rehash();
+    }
+}
